5d9c88cf32729cfa512f7f8422a62a3757145f3b
Doug Davis <ddavis@ddavis.io>
2019-10-31 13:00:10 -0400

    whoops

---
+++ ddavis-tex.el
@@ -0,0 +1,31 @@
+;;; ddavis-tex.el --- TeX stuff                      -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Doug Davis
+
+;; Author: Doug Davis <douglas.davis.092@gmail.com>
+;; Keywords: local
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(setq tex-fontify-script nil
+      font-latex-fontify-script nil)
+
+(provide 'ddavis-tex)
+;;; ddavis-tex.el ends here
