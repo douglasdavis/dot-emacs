834331bca02360a055770648f0c3cc7d2bb0f223
Doug Davis <ddavis@ddavis.io>
2019-11-07 16:58:08 -0500

    helm-rg macos kbd

--- ddavis-macos.el
+++ ddavis-macos.el
@@ -54,6 +54,7 @@
 (global-set-key (kbd "s-2") 'split-window-below)
 (global-set-key (kbd "s-3") 'split-window-right)
 (global-set-key (kbd "s-w") 'delete-window)
+(global-set-key (kbd "s-r") 'helm-rg)
 (global-set-key (kbd "s-.") 'other-window)
 (global-set-key (kbd "s-/") 'previous-buffer)
 (global-set-key (kbd "s-b") 'helm-buffers-list)
