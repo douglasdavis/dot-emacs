a14cb4e1aa2939ace9f3707cea271887962ee082
Doug Davis <ddavis@ddavis.io>
2020-06-03 10:38:15 -0400

    remove hook

--- email.el
+++ email.el
@@ -102,9 +102,6 @@
          ("M" . mu4e-action-view-in-w3m)
          ("j" . dd/mu4e-jump-via-comp-read))
   :config
-  (add-hook 'mu4e-view-mode-hook 'visual-fill-column-mode)
-  (add-hook 'mu4e-view-mode-hook
-            (lambda () (interactive) (setq-local fill-column 88)))
   (setq mu4e-mu-binary dd-mu-exe
         mu4e-change-filenames-when-moving t
         mu4e-get-mail-command "true"
