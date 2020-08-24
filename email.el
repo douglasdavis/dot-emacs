d9956e4c2e1cb462b6c48947df4d0719ed8de9f2
Doug Davis <ddavis@ddavis.io>
2020-08-24 14:04:04 -0400

    looks

--- email.el
+++ email.el
@@ -104,6 +104,7 @@
          ("M" . mu4e-action-view-in-w3m)
          ("j" . dd/mu4e-jump-via-comp-read))
   :config
+  (set-face-attribute 'mu4e-header-highlight-face nil :weight 'regular)
   (setq mu4e-mu-binary dd/mu-exe
         mu4e-change-filenames-when-moving t
         mu4e-get-mail-command "true"
