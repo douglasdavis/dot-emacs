9d8b09ff2fb66a23ffa8e36d3853849df3c1b243
Doug Davis <ddavis@ddavis.io>
2021-02-15 20:30:00 -0500

    early init

--- early-init.el
+++ early-init.el
@@ -3,14 +3,27 @@
 ;; 10GB GC threshold during init
 (setq gc-cons-threshold (* 10000 1024 1024))
 
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Loaded time: %s (with %d garbage collections)."
+                     (format "%.2f seconds"
+                             (float-time (time-subtract after-init-time
+                                                        before-init-time)))
+                     gcs-done)))
+
 (setq frame-inhibit-implied-resize t)
 (setq inhibit-startup-screen t)
 (setq custom-safe-themes t)
 
 (require 'package)
 (if (fboundp 'native-compile)
-    (setq package-user-dir (concat user-emacs-directory "elpa-" emacs-version "-native"))
-  (setq package-user-dir (concat user-emacs-directory "elpa-" emacs-version)))
+    (setq package-user-dir (concat user-emacs-directory
+                                   "elpa-"
+                                   emacs-version
+                                   "-native"))
+  (setq package-user-dir (concat user-emacs-directory
+                                 "elpa-"
+                                 emacs-version)))
 
 (provide 'early-init)
 ;;; early-init.el ends here
