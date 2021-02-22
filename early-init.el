fc4116954c7a7aa2d5ef37cc8a6f7fed43a94b6a
Doug Davis <ddavis@ddavis.io>
2021-02-22 12:28:52 -0500

    word

--- early-init.el
+++ early-init.el
@@ -5,7 +5,7 @@
 
 (add-hook 'emacs-startup-hook
           (lambda ()
-            (message "Loaded time: %s (with %d garbage collections)."
+            (message "Load time: %s (with %d garbage collections)."
                      (format "%.2f seconds"
                              (float-time (time-subtract after-init-time
                                                         before-init-time)))
