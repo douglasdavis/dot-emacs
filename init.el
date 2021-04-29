dfba573cc00f314c99d4a97c467d669712abc217
Doug Davis <ddavis@ddavis.io>
2021-04-29 14:03:38 -0400

    off

--- init.el
+++ init.el
@@ -845,9 +845,9 @@ Taken from post: https://zck.me/emacs-move-file"
   (when (version<= "28.0.50" emacs-version)
     (setq company-format-margin-function #'company-vscode-dark-icons-margin)))
 
-(use-package company-box
-  :ensure t
-  :hook (company-mode-hook . company-box-mode))
+;; (use-package company-box
+;;   :ensure t
+;;   :hook (company-mode-hook . company-box-mode))
 
 (use-package consult
   :ensure t
